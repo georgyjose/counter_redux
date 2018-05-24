034f2b5d9135ac35ce0350cc8e252cde04ecb9c0
Chachu <sharadjoe48@gmail.com>
2018-05-24 09:40:16 +0530

    Counter using react is complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import Counter from './Counter'
+import { Route } from 'react-router-dom'
 
-class App extends Component {
-  render() {
-    return (
-      
-    );
-  }
-}
+
+const App = () => <div class="ui container">
+
+  <Route path="/"
+    exact component = {
+      Counter
+    }
+  />
+
+</div>
 
 export default App;
--- src/counter.js
+++ src/counter.js
@@ -1,14 +1,52 @@
 import React, { Component } from 'react'
+import {
+    Button,
+    Form,
+    Grid,
+    Header,
+    Image,
+    Message,
+    Segment
+} from 'semantic-ui-react'
 
-class counter extends React.Component {
+class Counter extends React.Component {
 
     state = { count : 0 }
 
+    increment = () => {
+        this.setState({ count : this.state.count+1
+        });
+    }
+
+    decrement = () =>{
+        this.setState({ count : this.state.count-1
+        });
+    }
+
     render() {
         return (
-            <div>
-                
+            <div class="ui container middle aligned">
+                <Grid coloumns={3} >
+                <Grid.Row></Grid.Row>
+                <Grid.Row></Grid.Row>
+                <Grid.Row></Grid.Row>
+                <Grid.Row verticalAlign = "middle" >
+                    <Grid.Column width={3}>
+                    </Grid.Column>
+                    <Grid.Column width={10} verticalAlign="middle">
+                    <Header as='h1' textAlign="center" >{this.state.count}</Header>
+                        <Button.Group fluid>
+                            <Button onClick={this.decrement}>-</Button>
+                            <Button.Or />
+                            <Button onClick={this.increment}>+</Button>
+                        </Button.Group>
+                    </Grid.Column>
+                </Grid.Row>
+
+                </Grid>
             </div>
         )
     }
 }
+
+export default Counter
